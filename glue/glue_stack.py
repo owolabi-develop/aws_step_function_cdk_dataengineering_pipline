d9c3e041af593f2c5430ce4f1f716b3bbf39d388
owolabi-develop <owolabidevelop84@gmail.com>
2023-12-17 07:39:43 -0800

    updated glue stack

--- glue/glue_stack.py
+++ glue/glue_stack.py
@@ -47,11 +47,6 @@ class GlueCrawlerStack(Stack):
         
         
         
-        ## sqs que to trigger glue
-        glue_queue = _sqs.Queue(self, 'glue_queue')
-        raw_landing_bucket.add_event_notification(_s3.EventType.OBJECT_CREATED, s3n.SqsDestination(glue_queue))
-        
-        
         glue_role = _iam.Role(self, 'glue_role',
                       role_name='GlueRole',
                       description='Role for Glue services to access S3',
@@ -86,10 +81,7 @@ class GlueCrawlerStack(Stack):
                  database_name='property-database',
                  targets=_glue.CfnCrawler.TargetsProperty(
                      s3_targets=[_glue.CfnCrawler.S3TargetProperty(
-                         path=f's3://raw_landing_bucket/',
-                         event_queue_arn=glue_queue.queue_arn)]),
-                 recrawl_policy=_glue.CfnCrawler.RecrawlPolicyProperty(
-                     recrawl_behavior='CRAWL_EVENT_MODE'))
+                         path=f's3://{raw_landing_bucket}/')]))
 
         glue_job = _glue.CfnJob(self, 'glue_job',
                                 name='customer_data_job',
@@ -107,49 +99,8 @@ class GlueCrawlerStack(Stack):
                                   name='glue_workflow',
                                   description='Workflow to process the coffee data.')
 
-        _glue.CfnTrigger(self, 'glue_crawler_trigger',
-                        name='glue_crawler_trigger',
-                        actions=[_glue.CfnTrigger.ActionProperty(
-                            crawler_name='customer_crawler',
-                            notification_property=_glue.CfnTrigger.NotificationPropertyProperty(notify_delay_after=3),
-                            timeout=3)],
-                        type='EVENT',
-                        workflow_name=glue_workflow.name)
-
-        _glue.CfnTrigger(self, 'glue_job_trigger',
-                        name='glue_job_trigger',
-                        actions=[_glue.CfnTrigger.ActionProperty(
-                            job_name=glue_job.name,
-                            notification_property=_glue.CfnTrigger.NotificationPropertyProperty(notify_delay_after=3),
-                            timeout=3)],
-                        type='CONDITIONAL',
-                        start_on_creation=True,
-                        workflow_name=glue_workflow.name,
-                        predicate=_glue.CfnTrigger.PredicateProperty(
-                            conditions=[_glue.CfnTrigger.ConditionProperty(
-                                crawler_name='property_crawler',
-                                logical_operator='EQUALS',
-                                crawl_state='SUCCEEDED')]))
         
-        rule_role = _iam.Role(self, 'rule_role',
-                      role_name='EventBridgeRole',
-                      description='Role for EventBridge to trigger Glue workflows.',
-                      assumed_by=_iam.ServicePrincipal('events.amazonaws.com'),
-                      inline_policies={
-                          'eventbridge_policy': _iam.PolicyDocument(statements=[_iam.PolicyStatement(
-                              effect=_iam.Effect.ALLOW,
-                              actions=['events:*', 'glue:*'],
-                              resources=['*'])])})
-
-        _events.CfnRule(self, 'rule_s3_glue',
-                        name='rule_s3_glue',
-                        role_arn=rule_role.role_arn,
-                        targets=[_events.CfnRule.TargetProperty(
-                            arn=f'arn:aws:glue:{cdk.Aws.REGION}:{cdk.Aws.ACCOUNT_ID}:workflow/glue_workflow',
-                            role_arn=rule_role.role_arn,
-                            id=cdk.Aws.ACCOUNT_ID)],
-                        event_pattern={
-                            "detail-type": ["Object Created"],
-                            "detail": {
-                                "bucket": {"name": [f"{raw_landing_bucket.bucket_name}"]}},
-                            "source": ["aws.s3"]})
+        
+        
+        
+        
